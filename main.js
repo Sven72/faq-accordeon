a23ee7569da2a8a367169ba981c9c57a99273c3a
Sven Huff <info@sven-huff.de>
2020-11-05 20:10:21 +0100

    accordionElem

--- main.js
+++ main.js
@@ -2,10 +2,14 @@
 
 // https://medium.com/@vikash20186/creating-accordion-with-javascript-a33743655474
 
-let questionTab = document.querySelectorAll("#question");
+// let questionTab = document.querySelectorAll("#question");
 
-for (var i = 0; i < questionTab.length; i++) {
-  questionTab[i].addEventListener("click", () => {
-    console.log(parentElement);
-  });
-}
+// for (var i = 0; i < questionTab.length; i++) {
+//   questionTab[i].addEventListener("click", () => {
+//     console.log(parentElement);
+//   });
+// }
+
+let accordionElem = document.getElementById("accordion");
+
+// Ich muss noch eine Panel Klasse integrieren?
