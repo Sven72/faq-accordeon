d2d5191162d7bbd57bdc5b209cb05415dc422f65
Sven Huff <info@sven-huff.de>
2020-11-06 13:57:36 +0100

    Add accordion

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
-// https://medium.com/@wales149/how-to-create-an-accordion-using-html-css-javascript-3853cd02ce3f
+let question = document.querySelectorAll(".main-question");
 
-// https://medium.com/@vikash20186/creating-accordion-with-javascript-a33743655474
-
-var acc = document.getElementsByClassName("main-question");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function() {
-    this.classList.toggle("active");
-    // this.nextElementSibling.classList.toggle("show");
-  };
-
-  console.log("klicked " + i);
-}
+question.forEach(e => {
+  e.addEventListener("click", () => {
+    let answer = e.nextElementSibling;
+    answer.classList.toggle("active");
+  });
+});
