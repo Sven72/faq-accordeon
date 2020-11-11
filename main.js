ab28546265a3d9932e3366e5bece9a149fc61485
Sven Huff <info@sven-huff.de>
2020-11-11 11:43:16 +0100

    Finished mobile version

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let question = document.querySelectorAll(".main-question");
+let caret = document.getElementById("caret");
 
 question.forEach(e => {
   e.addEventListener("click", () => {
+    caret.classList.toggle("turn-svg");
     e.classList.toggle("highlight");
     let answer = e.nextElementSibling;
     answer.classList.toggle("active");
